8d133c54d46acf95d449be3bdcbe19e6efefb752
Matheus Ribeiro :D <github.matheusribeiro@gmail.com>
2022-04-02 17:39:54 -0300

    recuperando ofertas do tipo restaurante

--- src/app/restaurante/restaurante.component.ts
+++ src/app/restaurante/restaurante.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-restaurante',
   templateUrl: './restaurante.component.html',
-  styleUrls: ['./restaurante.component.css']
+  styleUrls: ['./restaurante.component.css'],
+  providers: [ OfertasService ]
 })
 export class RestauranteComponent implements OnInit {
 
-  constructor() { }
+  public ofertas?: Oferta[]
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertasService.getOfertasPorCategoria('restaurante')
+    .then(( ofertas: Oferta[] ) => {
+      this.ofertas = ofertas
+    })
   }
 
 }
