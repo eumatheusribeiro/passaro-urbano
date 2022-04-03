32567a30c9d4e85e9a4c05f930e374f022cb7432
Matheus Ribeiro :D <github.matheusribeiro@gmail.com>
2022-04-03 13:17:46 -0300

    ajustando a url de acesso a api

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Oferta } from "./shared/oferta.model"
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
@@ -10,19 +11,19 @@ export class OfertasService {
     }
     
     public getOfertas(): Promise<Array<Oferta>>{
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta)
     }   
 
     public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
                 .toPromise()
                 .then((resposta: any) => resposta)
     }
 
     public getOfertasPorId(id: number) : Promise<Oferta> {
-        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+        return this.http.get(`${URL_API}?id=${id}`)
                 .toPromise()
                 .then((resposta: any) => resposta[0]) 
     }
