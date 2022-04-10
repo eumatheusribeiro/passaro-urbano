ae1b11810bbe5fd5eb8f258100898225c8e434a0
Matheus Ribeiro :D <github.matheusribeiro@gmail.com>
2022-04-10 14:00:22 -0300

    refatorando formulario utilizando template forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { registerLocaleData } from "@angular/common";
 import localePt from "@angular/common/locales/pt";
+import { FormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -38,6 +39,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [ { provide: LOCALE_ID, useValue: 'pt-Br' } ],
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service';
 import { Pedido } from '../shared/pedido.model';
 
@@ -10,107 +11,24 @@ import { Pedido } from '../shared/pedido.model';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  @ViewChild('formulario') public formulario?: NgForm
+
   public idPedidoCompra?: string
-
-  //Pedido
-  public pedido: Pedido = new Pedido('', '', '', '')
-
-  public endereco: string = ''
-  public numero: string = '' 
-  public complemento: string = ''
-  public formaPagamento: string = ''
-
-  //controles de validação dos campos
-  public enderecoValido?: boolean
-  public numeroValido?: boolean
-  public complementoValido?: boolean
-  public formaPagamentoValido?: boolean
-
-  //estados primitivos do campo (pristine)
-  public enderecoEstadoPrimitivo: boolean = true
-  public numeroEstadoPrimitivo: boolean = true
-  public complementoEstadoPrimitivo: boolean = true
-  public formaPagamentoEstadoPrimitivo: boolean = true
-
-  //controlar botão confirmar compra
-  public formEstado?: string = 'disabled'
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit(): void {}
 
-  atualizaEndereco(endereco: string) : void {
-    this.endereco = endereco
+  confirmarCompra() : void {
 
-    this.enderecoEstadoPrimitivo = false
+    let pedido: Pedido = new Pedido(
+      this.formulario?.value.endereco,
+      this.formulario?.value.numero,
+      this.formulario?.value.complemento,
+      this.formulario?.value.formaPagamento
+      )
     
-    //se a string for maior que 3
-    if (this.endereco.length > 3) {
-      this.enderecoValido = true
-    } else {
-    this.enderecoValido = false
-    }
-
-    this.habilitaForm()
-  }
-
-  atualizaNumero(numero: string) : void {
-    this.numero = numero
-
-    this.numeroEstadoPrimitivo = false
-
-    //se a string nao for vazia
-    if (this.numero.length > 0) {
-      this.numeroValido = true
-    } else {
-      this.numeroValido = false
-    }
-  
-    this.habilitaForm()
-  }
-
-  atualizaComplemento( complemento : string) : void {
-    this.complemento = complemento
-    
-    this.complementoEstadoPrimitivo = false
-    
-    //se a string nao for vazia
-    if (this.complemento.length > 0) {
-      this.complementoValido = true
-    } 
-
-    this.habilitaForm()
-  }
-
-  atualizaFormaPagamento(formaPagamento : string) : void {
-    this.formaPagamento = formaPagamento
-
-    this.formaPagamentoEstadoPrimitivo = false
-    
-    //possui uma opção valida selecionada
-    if (this.formaPagamento.length > 0 && this.formaPagamento !== 'Selecione uma opção') {
-      this.formaPagamentoValido = true
-    } else {
-      this.formaPagamentoValido = false
-    }
-  
-    this.habilitaForm()
-  }
-
-  habilitaForm(): void {
-    if (this.enderecoValido === true && this.numeroValido === true && this.formaPagamentoValido ==true) {
-      this.formEstado = ''
-    } else {
-      this.formEstado = 'disabled'
-    }
-  }
-
-  confirmarCompra(): void {
-    this.pedido.endereco = this.endereco
-    this.pedido.numero = this.numero
-    this.pedido.complemento = this.complemento
-    this.pedido.formaPagamento = this.formaPagamento
-    this.ordemCompraService.efetivarCompra(this.pedido)
+    this.ordemCompraService.efetivarCompra(pedido)
     .subscribe((idPedido: any) => {
       this.idPedidoCompra = idPedido
     })
